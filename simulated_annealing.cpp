0c52a6025b2c6ea8050c55edef76655cb1c8065d
Frederico <fredericoboller@outlook.com>
2025-05-28 22:45:11 -0300

    setup init

---
+++ simulated_annealing.cpp
@@ -0,0 +1,19 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+typedef long long ll;
+
+signed main(){
+
+    fstream stream;
+
+    stream.open("teste1.txt");
+
+    int n,m;
+
+    stream >> n >> m; 
+
+
+    return 0;
+}
