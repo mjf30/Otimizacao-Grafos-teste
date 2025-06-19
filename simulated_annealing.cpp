638eaacdefd72adcb770886ffb3c81e7637fc61e
raphaelabarbosa <raphaelasb07@gmailcom>
2025-06-19 18:38:32 -0300

    Leitura dos arquivos

--- simulated_annealing.cpp
+++ simulated_annealing.cpp
@@ -1,13 +1,18 @@
-#include "bits/stdc++.h"
+#include <iostream>
+#include <fstream>     
+#include <string>      
+#include <vector>      
+#include <utility>
+
 using namespace std;
 
 typedef long long ll;
 
 signed main(){
     //Entrada (Cenário/tipo/)
-    const int total_arquivos = 10;git push origin main
-    for(int i = 1; i < total_arquivos; i++){
-        string nome_arquivo = "kpfs_"+i+".txt";
+    const int total_arquivos = 10;
+    for(int i = 1; i <= total_arquivos; i++){
+        string nome_arquivo = "kpfs_" + to_string(i)+ ".txt";
 
         ifstream arquivo(nome_arquivo);
 
@@ -16,8 +21,8 @@ signed main(){
             continue;
         }
 
-        int itens, sets, capacity;
-        arquivo >> itens >> sets >> capacity;
+        int itens, quant_sets, capacity;
+        arquivo >> itens >> quant_sets >> capacity;
 
         vector<int> lucro(itens),peso(itens);
 
@@ -25,12 +30,17 @@ signed main(){
         for(int j = 0; j < itens; j++) arquivo >> peso[j];
 
         int permitido_set, custo_set, itens_set;
-        //declarar estrutura que guarda itens do set
-        for(int j = 0; j < sets; j++){
+
+        vector<vector<int> > set(itens);
+        vector<pair<int,int> > inf_sets(quant_sets);
+
+        for(int j = 0; j < quant_sets; j++){
             arquivo >> permitido_set >> custo_set >> itens_set;
+            inf_sets[j] = make_pair(permitido_set,custo_set);
+
             for(int k = 0; k < itens_set; k++){
-                int iten; arquivo >> item;
-                //Guardar na estrutura 
+                int item; arquivo >> item;
+                set[item].push_back(j);
             }
         }
     }
