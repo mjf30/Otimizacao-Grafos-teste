840fcf8e29a433993e196329d5b034103f0d668a
raphaelabarbosa <raphaelasb07@gmailcom>
2025-06-19 19:53:37 -0300

    Leitura dos arquivos e comentários

--- simulated_annealing.cpp
+++ simulated_annealing.cpp
@@ -3,53 +3,55 @@
 #include <string>      
 #include <vector>      
 #include <utility>
+#include <time.h>
 
 using namespace std;
 
 typedef long long ll;
 
 signed main(){
-    //Entrada (Cenário/tipo/)
-    const int scenarios = 4;
+    //Leitura dos arquivos
+    const int cenarios = 4;
     vector<string> pasta_tipos = {"correlated_sc", "fully_correlated_sc", "not_correlated_sc"};
     vector<string> pasta_tamanho = {"300","500","700","800","1000"};
 
-    for (int s_pasta = 1; s_pasta <= scenarios; s_pasta++) {
+    for (int s_pasta = 1; s_pasta <= cenarios; s_pasta++) {
         for (const auto& ss_pasta : pasta_tipos) {
             for(const auto& sss_pasta : pasta_tamanho){   
                 const int total_arquivos = 10;
                 for(int i = 1; i <= total_arquivos; i++){
+
                     string diretorio_arquivo = "instances/scenario"+to_string(s_pasta)+"/"+ss_pasta+to_string(s_pasta)+"/"+sss_pasta+"/"+"kpfs_" + to_string(i)+ ".txt";
-        
                     ifstream arquivo(diretorio_arquivo);
-        
                     if (!(arquivo.is_open())) {
-                        cout << "Erro ao abrir o arquivo: " << diretorio_arquivo << endl;
-                        continue;
+                        cout << "Erro ao abrir o arquivo: " << diretorio_arquivo << endl;continue;
                     }
-        
-                    int itens, quant_sets, capacity;
-                    arquivo >> itens >> quant_sets >> capacity;
-        
-                    vector<int> lucro(itens),peso(itens);
-        
+
+                    //Início da leitura
+                    //cout << "Estou lendo o arquivo:" << diretorio_arquivo << endl;
+                    int itens, quant_conj, capacidade; arquivo >> itens >> quant_conj >> capacidade;
+    
+                    vector<int> lucro(itens),peso(itens); //Lucro e peso do item i
                     for(int j = 0; j < itens; j++) arquivo >> lucro[j];
                     for(int j = 0; j < itens; j++) arquivo >> peso[j];
         
-                    int permitido_set, custo_set, itens_set;
+                    int lim_conj, penalidade_conj, itens_conj;
+                    vector<vector<int> > conj(itens); //Lista de conjunto que o item i pertence (conj[i] = lista)
+                    vector<pair<int,int> > inf_conj(quant_conj);//(Limite de itens na solução,Penalidade) do conjunto i
+
+                    for(int j = 0; j < quant_conj; j++){
+                        arquivo >> lim_conj >> penalidade_conj >> itens_conj;
+                        inf_conj[j] = make_pair(lim_conj,penalidade_conj);
         
-                    vector<vector<int> > set(itens);
-                    vector<pair<int,int> > inf_sets(quant_sets);
-        
-                    for(int j = 0; j < quant_sets; j++){
-                        arquivo >> permitido_set >> custo_set >> itens_set;
-                        inf_sets[j] = make_pair(permitido_set,custo_set);
-        
-                        for(int k = 0; k < itens_set; k++){
+                        for(int k = 0; k < itens_conj; k++){
                             int item; arquivo >> item;
-                            set[item].push_back(j);
+                            conj[item].push_back(j);
                         }
                     }
+
+                    //Fim na leitura
+
+                    //Chamada da função
                 }
             }
         }
