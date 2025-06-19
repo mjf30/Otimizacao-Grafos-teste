297cde48cf1be8cf046e67e15522ec8ce6297372
raphaelabarbosa <raphaelasb07@gmailcom>
2025-06-19 16:33:22 -0300

    Leitura dos arquivos

--- simulated_annealing.cpp
+++ simulated_annealing.cpp
@@ -1,19 +1,39 @@
 #include "bits/stdc++.h"
-
 using namespace std;
 
 typedef long long ll;
 
 signed main(){
+    //Entrada (Cenário/tipo/)
+    const int total_arquivos = 10;git push origin main
+    for(int i = 1; i < total_arquivos; i++){
+        string nome_arquivo = "kpfs_"+i+".txt";
 
-    fstream stream;
+        ifstream arquivo(nome_arquivo);
 
-    stream.open("teste1.txt");
+        if (!(arquivo.is_open())) {
+            cout << "Erro ao abrir o arquivo: " << nome_arquivo << endl;
+            continue;
+        }
 
-    int n,m;
+        int itens, sets, capacity;
+        arquivo >> itens >> sets >> capacity;
 
-    stream >> n >> m; 
+        vector<int> lucro(itens),peso(itens);
 
+        for(int j = 0; j < itens; j++) arquivo >> lucro[j];
+        for(int j = 0; j < itens; j++) arquivo >> peso[j];
+
+        int permitido_set, custo_set, itens_set;
+        //declarar estrutura que guarda itens do set
+        for(int j = 0; j < sets; j++){
+            arquivo >> permitido_set >> custo_set >> itens_set;
+            for(int k = 0; k < itens_set; k++){
+                int iten; arquivo >> item;
+                //Guardar na estrutura 
+            }
+        }
+    }
 
     return 0;
 }
